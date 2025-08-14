18037b2e1bd607bd1f4102962389d7854b27b186
Evgeniy Zherzdev <zherzdev.en@sibvaleo.com>
2025-08-14 13:14:27 +0300

    Linked List Cycle II

--- main.go
+++ main.go
@@ -24,3 +24,31 @@ func hasCycle(head *ListNode) bool {
 	}
 	return false
 }
+
+func detectCycle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+	slow, fast := head, head
+	hasCycle := false
+
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if fast == slow {
+			hasCycle = true
+			break
+		}
+	}
+
+	if !hasCycle {
+		return nil
+	}
+
+	fast = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
